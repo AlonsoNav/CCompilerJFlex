241179156433893a8e680d6ee97888d25a3aac26
rocket310 <gomezacunav@gmail.com>
2024-08-29 23:33:16 -0600

    Update TokenType.java

--- src/scanner/TokenType.java
+++ src/scanner/TokenType.java
@@ -1,5 +1,13 @@
 package scanner;
 
 public enum TokenType {
-    KEYWORD, ID, OPERATOR,
+    KEYWORD, ID, OPERATOR,NUMERIC_LITERAL,
+    STRING_LITERAL,
+    INTEGER_LITERAL,
+    CHAR_LITERAL,
+    BINARY_LITERAL, 
+    OCTAL_LITERAL,  
+    HEX_LITERAL,
+    DOUBLE_LITERAL;
+
 }
