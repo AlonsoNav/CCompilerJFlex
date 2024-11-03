1e3d2f4c43d1b365a83470c8d1630495dfc563c3
rocket310 <gomezacunav@gmail.com>
2024-11-02 23:26:44 -0600

    Revert "switch"
    This reverts commit da44977f02068f5e77b433d8a9531b4fbba30abd.

--- parser.c
+++ parser.c
@@ -3,11 +3,12 @@ const long c = 4;
 long int d = 5;
 int short e = 6;
 
-
-
 void PruebaSinParametro(int a) {
     int a = 0;
     if (a + b > c && b - a < c) {
+        return;
+    }
+    if (a + b > c && b - a) {
         return;
     }
 }
@@ -19,22 +20,5 @@ int PruebaConParametro(int a) {
 int main() {
     long bigNum = 1.23E30L;
     short int a = 0;
-    int b;
-
-    b = a;
-    switch (a) {
-        case 'ab':
-            break;
-        case 'a':
-            a = 5;
-            break;
-        case 'psd':
-            a = 8;
-            break;
-        default:
-            a = 20;
-            break;
-    }
-
     return;
 }
