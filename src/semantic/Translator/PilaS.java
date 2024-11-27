af31b254fb5b70896ddc57f34f92f45d4a47a232
Alonso <106555422+AlonsoNav@users.noreply.github.com>
2024-11-27 12:19:31 -0600

    Refactor: stack changes

--- src/semantic/Translator/PilaS.java
+++ src/semantic/Translator/PilaS.java
@@ -1,72 +1,36 @@
 package semantic.Translator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class PilaS {
     
-    //lista de registros
-    private List<RS> pilaS;
+    private LinkedList<RS> pilaS;
 
     public PilaS() {
-        this.pilaS = new ArrayList<>();
+        this.pilaS = new LinkedList<RS>();
     }
 
-    public void clear() {
-        pilaS.clear();
+    public void push(RS register) {
+        pilaS.push(register);
     }
 
-
-    // va a guardar en la posicion 0 de la pila
-    public void push(String value) {
-        pilaS.add(new RS(value, "", ""));
-    }
-
-
-
-    public void push_end(String value)
-    {
-        pilaS.add(pilaS.size(), new RS(value, "", ""));
-    }
-
-    /*public void pop() {
-        pilaS.remove(0);
-    }*/
-
-    // Elimina y devuelve el elemento del tope de la pila
     public RS pop() {
-        if (pilaS.isEmpty()) {
-            throw new IllegalStateException("La pila está vacía");
-        }
-        return pilaS.remove(pilaS.size() - 1);
-    }
-    
-    // Elimina y devuelve el elemento del tope de la pila (equivalente a pop)
-    public void popTope() {
-        try {
-            pilaS.remove(pilaS.size() - 1);
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Error: La pila está vacía");
-        }
+        return pilaS.pop();
     }
 
-    // Devuelve el elemento del tope de la pila sin eliminarlo
     public RS peek() {
-        if (pilaS.isEmpty()) {
-            throw new IllegalStateException("La pila está vacía");
-        }
-        return pilaS.get(pilaS.size() - 1);
+        return pilaS.peek();
     }
 
-
-
-    //imprimir la pila
-    public void printPila() {
-        System.out.println("Pila S:");
-        for (int i = 0; i < pilaS.size(); i++) {
-            System.out.println("Registro: " + pilaS.get(i).getValue());
+    public Boolean searchContext(String context) {
+        ListIterator<RS> iterator = pilaS.listIterator(pilaS.size());
+        while (iterator.hasPrevious()) {
+            RS register = iterator.previous();
+            if (register.getType().equals(context)) {
+                return true;
+            }
         }
+        return false;
     }
-
-
 }
--- src/semantic/Translator/RS.java
+++ src/semantic/Translator/RS.java
@@ -1,15 +1,18 @@
 package semantic.Translator;
 
+import java.util.ArrayList;
+
 public class RS {
-    
     private String value;
     private String code;
     private String type;
+    private ArrayList<String> labels;
 
     public RS(String value, String code, String type) {
         this.value = value;
         this.code = code;
         this.type = type;
+        this.labels = new ArrayList<String>();
     }
 
     public String getValue() {
@@ -26,5 +29,13 @@ public class RS {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public void addLabel(String label) {
+        labels.add(label);
+    }
+
+    public String getLabel(int index) {
+        return labels.get(index);
     }
 }
